837a319f795e70df29f2e86c7b8dcc3229e83d9a
mckayhyorgason <mckay.yorgason21@gmail.com>
2026-02-13 16:31:01 -0700

    set up sqlite

--- Mission06_Yorgason/obj/Debug/net10.0/Mission06_Yorgason.AssemblyInfo.cs
+++ Mission06_Yorgason/obj/Debug/net10.0/Mission06_Yorgason.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission06_Yorgason")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62f3af4b5c55a995b37276a2b34fa40160bc4009")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission06_Yorgason")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission06_Yorgason")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
