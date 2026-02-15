cd74a71afffeae85b3545a158a0d415816833407
mckayhyorgason <mckay.yorgason21@gmail.com>
2026-02-14 18:39:48 -0700

    set up migrations

--- Mission06_Yorgason/obj/Debug/net10.0/Mission06_Yorgason.AssemblyInfo.cs
+++ Mission06_Yorgason/obj/Debug/net10.0/Mission06_Yorgason.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission06_Yorgason")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+837a319f795e70df29f2e86c7b8dcc3229e83d9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c754ebcd777b2e185c8fdd5e2c74f39bb3dcc70")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission06_Yorgason")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission06_Yorgason")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
