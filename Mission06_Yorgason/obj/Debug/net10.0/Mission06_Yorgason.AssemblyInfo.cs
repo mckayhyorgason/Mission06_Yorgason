5dba131b8afea6d321ae5b6efde882618c64cbe7
mckayhyorgason <mckay.yorgason21@gmail.com>
2026-02-17 09:27:59 -0700

    styling add movie

--- Mission06_Yorgason/obj/Debug/net10.0/Mission06_Yorgason.AssemblyInfo.cs
+++ Mission06_Yorgason/obj/Debug/net10.0/Mission06_Yorgason.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission06_Yorgason")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67cd76f4b748b6dcbf669573d14c695602617a96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b16a37184d23e6e5bdc732087de20604eb36fe1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission06_Yorgason")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission06_Yorgason")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
